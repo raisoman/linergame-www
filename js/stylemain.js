cefefa5fd5b9f066fe3367599656d1807ecaa32c
Nicolas Guilbert <nicolas@ange.dk>
2016-02-08 13:51:03 +0100

    Factor out styling of page content

---
+++ js/stylemain.js
@@ -0,0 +1,5 @@
+var styleMain = function() {
+    document.getElementById('content').className += ' mdl-layout__content page-content\
+        mdl-cell mdl-cell--12-col mdl-color-text--grey-800 article-section\
+        article-content mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-grid';
+}
